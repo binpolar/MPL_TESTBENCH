84c94a558c0c3e97a0bcec0b91f0899aa8eec010
Dario Cardajoli <dario.cardajoli@monozukuri.eu>
2026-04-01 15:24:33 +0200

    modified:   MPL 	modified:   main.c

--- main.c
+++ main.c
@@ -1,26 +1,47 @@
 #include <stdio.h>
 #include "MPL/mpl_utils.h"
 #include "MPL/mpl_hashmap.h"
+#include "MPL/mpl_heap.h"
 #include "MPL/MPL.h"
 
-void test_edges();
+void test_edges_creation(uint32_t addr1, uint32_t addr2);
+void test_nodes_creation(uint32_t addr);
+void test_pathfinding(uint32_t n1, uint32_t n2);
 
 int main()
 {
     printf("---------MPL TESTBENCH---------\n\r");
 
     init_maps();
+    init_heap();
 
-    test_edges(0x0, 0x1);
-    test_edges(0x2, 0x3);
+    test_nodes_creation(0x0);
+    test_nodes_creation(0x1);
+
+    test_edges_creation(0x0, 0x1);
+
+    test_pathfinding(0x0, 0x1);
 
     return 0;
 }
 
-void test_edges(uint32_t addr1, uint32_t addr2)
+void test_nodes_creation(uint32_t addr)
+{
+    printf("---------TESTING NODE CREATION---------\n\r");
+    if (mpl_create_node_if_not_exists(addr))
+    {
+        printf("Created nod, checking if it exists: %s\n\r", mpl_node_exists(addr) ? "SUCCESS" : "FAILED!!!");
+    }
+    else
+    {
+        printf("Node creation failed!!!");
+    }
+}
+
+void test_edges_creation(uint32_t addr1, uint32_t addr2)
 {
     printf("---------TESTING EDGE CREATION---------\n\r");
-    mpl_node_t tmp1, tmp2;
+    mpl_node_t tmp1 = {0}, tmp2 = {0};
     mpl_edge_t *edge, *egde;
 
     tmp1.address = addr1;
@@ -32,11 +53,11 @@ void test_edges(uint32_t addr1, uint32_t addr2)
         edge = mpl_get_edge(&tmp2, &tmp1);
         if (edge != NULL)
         {
-            printf("Edge values\n\r N1:%u, N2:%u, Q:%u\n\r", edge->nodes[0], edge->nodes[1], edge->link_quality);
+            printf("Edge values\n\r N1:%u, N2:%u, Q:%u\n\r", edge->nodes[0]->address, edge->nodes[1]->address, edge->link_quality);
         }
         else
         {
-            printf("COULDNT READ BACK EDGE!!!\n\r");
+            printf("Couldnt read back edge!!!\n\r");
         }
     }
     else
@@ -54,7 +75,39 @@ void test_edges(uint32_t addr1, uint32_t addr2)
         }
         else
         {
-            printf("Edge update %s", egde->link_quality == 0 ? "SUCCESS" : "FAILED!!!");
+            printf("Edge update %s\n\r", egde->link_quality == 0 ? "SUCCESS" : "FAILED!!!");
         }
+    }
+}
+
+void test_pathfinding(uint32_t n1, uint32_t n2)
+{
+    printf("---------TESTING PATHFINDING---------\n\r");
+
+    mpl_create_node_if_not_exists(n1);
+    mpl_create_node_if_not_exists(n2);
+
+    mpl_node_t *src = mpl_get_node(n1);
+    mpl_node_t *dest = mpl_get_node(n2);
+
+    mpl_create_or_update_edge(src, dest, 100);
+    mpl_create_or_update_edge(dest, src, 100);
+
+    if (src == NULL || dest == NULL)
+    {
+        printf("Couldnt get end nodes!!!\n\r");
+    }
+
+    mpl_route_t tmp;
+    if (mpl_find_route(src, dest, &tmp))
+    {
+        for (uint8_t i = 0; i < tmp.hop_count; i++)
+        {
+            printf("Hop %u:%u\n\r", i, tmp.hops[i]);
+        }
+    }
+    else
+    {
+        printf("Pathfinding failed!!!\n\r");
     }
 }
